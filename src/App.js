ca8204ccd4dadc3bb08bf3fe1c76b9659da9ebb8
Konrad Kapica <konradkapica@gmail.com>
2022-11-12 23:46:11 +0100

    task list without interactions yet

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Header from "./Header";
 import Container from "./Container";
 
 const tasks = [
-  //{ id: 1, content: "przejść na Reacta", done: false },
-  //{ id: 2, content: "naprawić plecy", done: true },
+  { id: 1, content: "przejść na Reacta", done: false },
+  { id: 2, content: "naprawić plecy", done: true },
 ];
 
 const hideDoneTasks = false;
