cdbf852ee804b3abb58a13ddce2927fe3dc769ec
Konrad Kapica <konradkapica@gmail.com>
2023-01-09 21:48:18 +0100

    Search moved to TasksPage

---
+++ src/features/tasks/TasksPage/Search/styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    background-color: white;
+    padding: 20px;
+    display: grid;
+    grid-template-columns: 1fr;
+`;
