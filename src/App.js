315ca43695318e57d19c22d9a78cff74d5532b47
Konrad Kapica <konradkapica@gmail.com>
2023-01-09 21:50:50 +0100

    react-router implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import Tasks from "./features/tasks/Tasks";
+import { Switch, Route, HashRouter, Redirect } from "react-router-dom";
+import AuthorPage from "./features/author/AuthorPage";
+import TasksPage from "./features/tasks/TasksPage";
+import TaskPage from "./features/tasks/TaskPage";
+import { StyledNavLink } from "./styled";
 
 function App() {
     return (
-        <Tasks />
+        <HashRouter>
+            <nav>
+                <ul>
+                    <li>
+                        <StyledNavLink to="/zadania">Zadania</StyledNavLink>
+                    </li>
+                    <li>
+                        <StyledNavLink to="/autor">O autorze</StyledNavLink>
+                    </li>
+                </ul>
+            </nav>
+            <Switch>
+                <Route path="/zadania/:id">
+                    <TaskPage />
+                </Route>
+                <Route path="/zadania">
+                    <TasksPage />
+                </Route>
+                <Route path="/autor">
+                    <AuthorPage />
+                </Route>
+                <Route path="/">
+                    <Redirect to="/zadania" />
+                </Route>
+            </Switch>
+        </HashRouter>
     );
 }
 
