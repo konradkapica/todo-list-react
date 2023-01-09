a37ec0546252a38e1fc59b526e00802f82760fef
Konrad Kapica <konradkapica@gmail.com>
2023-01-09 21:48:04 +0100

    Form moved to TasksPage

---
+++ src/features/tasks/TasksPage/Form/index.js
@@ -0,0 +1,54 @@
+import { useState, useRef } from "react";
+import { useDispatch } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";
+import { addTask } from "../../tasksSlice";
+import { StyledForm, Button } from "./styled"
+import Input from "../../Input";
+
+const Form = () => {
+    const [newTaskContent, setNewTaskContent] = useState("");
+    const inputRef = useRef(null);
+
+    const dispatch = useDispatch();
+
+    const trimmedNewTaskContent = newTaskContent.trim();
+
+    const inputFocus = () => {
+        inputRef.current.focus();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        if (!trimmedNewTaskContent) {
+            setNewTaskContent("");
+            return;
+        }
+
+        dispatch(addTask({
+            content: trimmedNewTaskContent,
+            done: false,
+            id: nanoid(),
+        }));
+
+        setNewTaskContent("");
+    };
+
+    return (
+        <StyledForm onSubmit={onFormSubmit}>
+            <Input
+                value={newTaskContent}
+                placeholder="Co jest do zrobienia?"
+                autoFocus
+                onChange={({ target }) => setNewTaskContent(target.value)}
+                ref={inputRef}
+            />
+            <Button
+                onClick={inputFocus}
+            >
+                Dodaj zadanie
+            </Button>
+        </StyledForm>
+    )
+};
+
+export default Form;
