cb896696fcf79f195808d29366b5bbb8a98b7be3
Konrad Kapica <konradkapica@gmail.com>
2023-01-10 12:41:00 +0100

    text added to AuthorPage

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -1,7 +1,7 @@
 import { Container } from "../../common/Container/styled";
 import Header from "../../common/Header";
 import Section from "../../common/Section";
-import { Wrapper } from "./styled";
+import { Wrapper, Paragraph } from "./styled";
 
 function AuthorPage() {
     return (
@@ -11,7 +11,12 @@ function AuthorPage() {
                 title="Konrad Kapica"
                 body={
                     <Wrapper>
-                        <p>nanan</p>
+                        <Paragraph>
+                            <strong>Moją pasją jest muzyka.</strong> Od 6 roku życia zajmuję się nią codziennie. Gram na instrumentach perkusyjnych od 15 lat. Interesuje mnie głównie <i>jazz, funky i czasem pop.</i> Grając z orkiestrą zwiedziłem kilka kontynentów, co jeszcze bardziej motywuje mnie do pogłębiania pasji 🏝️
+                        </Paragraph>
+                        <Paragraph>
+                            Poza muzyką jestem również <strong>aktorem i fotografem.</strong> <em>Sztuka</em> znaczy dla mnie o wiele więcej niż można przypuszczać! 🚀
+                        </Paragraph>
                     </Wrapper>
                 }
             />
--- src/features/author/styled.js
+++ src/features/author/styled.js
@@ -4,3 +4,7 @@ export const Wrapper = styled.div`
     background-color: white;
     padding: 20px;
 `
+
+export const Paragraph = styled.p`
+ font-size: 16px;
+`;
