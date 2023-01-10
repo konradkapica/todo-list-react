a6dd7a2240dd27e3af1273ba853788ff45a26c2d
Konrad Kapica <konradkapica@gmail.com>
2023-01-10 14:17:46 +0100

    queryParameters.js implemented

---
+++ src/features/tasks/TasksPage/queryParameters.js
@@ -0,0 +1,23 @@
+import { useHistory, useLocation } from "react-router-dom";
+
+export const useQueryParameter = (searchQueryParamName) => {
+    const location = useLocation();
+    return new URLSearchParams(location.search).get(searchQueryParamName);
+};
+
+export const useReplaceQueryParameter = () => {
+    const location = useLocation();
+    const history = useHistory();
+
+    return ({ key, value }) => {
+        const searchParams = new URLSearchParams(location.search);
+
+        if (value) {
+            searchParams.set(key, value)
+        } else {
+            searchParams.delete(key)
+        }
+
+        history.push(`${location.pathname}?${searchParams.toString()}`);
+    };
+};
